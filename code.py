5fc551428b74a60c64d4db5aa14f58fce7e7bb0f
ahmedhat1 <ahmed.sadekibrahim@gmail.com>
2023-01-21 21:24:47 +0100

    api loads 1000 images of dogs cats and alligators and saves them in files

--- code.py
+++ code.py
@@ -1,26 +1,38 @@
 from flickrapi import FlickrAPI
+import urllib.request
+from PIL import Image
 
 key = "b65896c797abd5c49080ba666165e762"
 secret = "860f6f7ba08fce00"
 
-flickr = FlickrAPI(key, secret, format='parsed-json')
+# flickr = FlickrAPI(key, secret, format='parsed-json')
 
-def get_photos(flickr, num=1000):
-    result  = flickr.photos.search(
-        text = 'dog',
-        per_page = num, # Default 100, maximum allowed: 500.
-        media = 'photos', # all (default), photos or videos
-        content_type = 1, #just photos (no screenshots nor 'other')
-        sort = 'relevance',
-        privacy_filter = 1, #public photos
-        safe_search = 1
-    )
-    return result
 
-result = get_photos(flickr, num = 10)
-#print(result['photos']['photo'][0]
-#print(result)
+flickr = FlickrAPI(key, secret, format='etree')
+keyword = ['dog', 'cat', 'alligator']
 
-photo = result['photos']['photo'][0]
-url = f"https://live.staticflickr.com/{photo['server']}/{photo['id']}_{photo['secret']}.jpg"
-print(url)
+for i in range(3):
+
+    photos = flickr.walk(text=keyword[i],
+                         tag_mode='all',
+                         tags=keyword[i],
+                         extras='url_c',
+                         per_page=1000,
+                         sort='relevance')
+
+    urls = []
+    j = 0
+    for _, photo in enumerate(photos):
+        url = photo.get('url_c')
+        if url != None:
+            j = j + 1
+            urls.append(url)
+
+        if j == 1000:
+            break
+
+    for k in range(1000):
+        urllib.request.urlretrieve(urls[k], f'{keyword[i]}s/{keyword[i]}_{k}.jpg')
+        image = Image.open(f'{keyword[i]}s/{keyword[i]}_{k}.jpg')
+        image = image.resize((256, 256))
+        image.save(f'{keyword[i]}s/{keyword[i]}_{k}.jpg')
